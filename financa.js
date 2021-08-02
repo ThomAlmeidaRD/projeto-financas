b3bef7008b061f62f318fbe3ca03de9f2ec2d9ca
ThomAlmeidaRD <thomas.almeidard@gmail.com>
2021-08-02 17:50:58 -0300

    javascritp otimizado na função 'saude financeira'

--- financa.js
+++ financa.js
@@ -5,42 +5,40 @@ const usuario = {
     salario: 800,
     saldo: 1500.00,
     despesaLuz: 450.00,
-    despesaAgua:250.00,
-    despesaML:109.15,
-    despesaVP:51.39,
-    despesaCC:52.14,
-    gastos: 0,
+    despesaAgua: 250.00,
+    despesaML: 109.15,
+    despesaVP: 51.39,
+    despesaCC: 52.14,
+    gastos: 200,
 
-    totalDespesas: function(){
+    totalDespesas: function () {
 
         this.gastos = this.despesaAgua + this.despesaLuz + this.despesaML + this.despesaCC + this.despesaVP
         console.log("Total: " + this.totalDespesas)
 
     },
 
-    chekUp: function(){
+    chekUp: function () {
         console.log("Usuario:")
         console.log(this.nome)
         console.log(this.salario)
         console.log(this.saldo)
     },
 
-    showSaldo: function(){
-        console.log("O saldo da conta é de: R$" + this.saldo )
+    showSaldo: function () {
+        alert("O saldo da conta é de: R$" + this.saldo)
     },
 
-    showSalario: function(){
-        console.log("O salário da conta é de: R$" + this.salario)
+    showSalario: function () {
+        alert("O salário da conta é de: R$" + this.salario)
     },
 
-    saudeFinanceira: function(){
+    saudeFinanceira: function () {
 
-        if( this.despesaFull >= this.salario){
-            console.log("Salario:" + this.salario)
-            console.log("Gastos:" + this.despesaFull)
-            console.log("Você gasta mais do que ganha!!")
-        }else{
-            console.log("Saúde financeira está OK! ")
+        if (this.gastos >= this.salario) {
+            sf.innerText = "Você gasta mais do que ganha!!"
+        } else {
+            sf.innerHTML = "<p>Saúde financeira está OK! &#9989</p>"
         }
 
     }
@@ -55,3 +53,5 @@ sd.innerText = "R$ " + usuario.saldo
 
 var dp = document.getElementById("despesa")
 dp.innerText = "R$" + usuario.gastos
+
+var sf = document.getElementById('saude-financeira')
